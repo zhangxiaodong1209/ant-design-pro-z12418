eeccaa2038f5cc5f033b59eadd7753a46340418d
zhangxiaodong (INT) <zhang.xiaodong@h3c.com>
2019-02-02 17:28:02 +0800

    first commit

---
+++ src/layouts/UserLayout.js
@@ -0,0 +1,55 @@
+import React, { Fragment } from 'react';
+import { formatMessage, FormattedMessage } from 'umi/locale';
+import Link from 'umi/link';
+import { Icon } from 'antd';
+import GlobalFooter from '@/components/GlobalFooter';
+import SelectLang from '@/components/SelectLang';
+import styles from './UserLayout.less';
+import logo from '../assets/h3c.svg';
+
+const links = [
+  // {
+  //   key: 'help',
+  //   title: formatMessage({ id: 'layout.user.link.help' }),
+  //   href: '',
+  // },
+  // {
+  //   key: 'privacy',
+  //   title: formatMessage({ id: 'layout.user.link.privacy' }),
+  //   href: '',
+  // },
+  // {
+  //   key: 'terms',
+  //   title: formatMessage({ id: 'layout.user.link.terms' }),
+  //   href: '',
+  // },
+];
+
+const copyright = (
+  <Fragment>
+    Copyright <Icon type="copyright" /> <FormattedMessage id="layout.user.copyright.com" />
+  </Fragment>
+);
+
+const UserLayout = ({ children }) => (
+  // @TODO <DocumentTitle title={this.getPageTitle()}>
+  <div className={styles.container}>
+    <div className={styles.lang}>
+      <SelectLang />
+    </div>
+    <div className={styles.content}>
+      <div className={styles.top}>
+        <div className={styles.header}>
+          <Link to="/">
+            <img alt="logo" className={styles.logo} src={logo} />
+            <span className={styles.title}>Smart Apartment</span>
+          </Link>
+        </div>
+      </div>
+      {children}
+    </div>
+    <GlobalFooter links={links} copyright={copyright} />
+  </div>
+);
+
+export default UserLayout;
